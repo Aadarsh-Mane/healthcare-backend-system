ab60553143d654bd554c336e635f872df45dbe39
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-03-06 17:31:07 +0530

    ports

--- tambebackend/index.js
+++ tambebackend/index.js
@@ -15,7 +15,7 @@ import { Server } from "socket.io";
 import http from "http";
 import { socketHandler } from "./socketHandler.js";
 import fs from "fs";
-const port = 9000;
+const port = 3004;
 
 const app = express();
 const server = http.createServer(app);
